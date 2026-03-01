95c86a93c987e2a639361f3802b63ce7f7ff0afb
RADHA SHREE M <radhashri1999@gmail.com>
2026-03-01 20:41:26 +0000

    Application Runtime Error RoleFlow encountered a critical client-side ex

--- src/firebase/firestore/use-collection.tsx
+++ src/firebase/firestore/use-collection.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { useState, useEffect } from 'react';
@@ -108,7 +109,7 @@ export function useCollection<T = any>(
     return () => unsubscribe();
   }, [memoizedTargetRefOrQuery]); // Re-run if the target query/reference changes.
   if(memoizedTargetRefOrQuery && !memoizedTargetRefOrQuery.__memo) {
-    throw new Error(memoizedTargetRefOrQuery + ' was not properly memoized using useMemoFirebase');
+    throw new Error(String(memoizedTargetRefOrQuery) + ' was not properly memoized using useMemoFirebase');
   }
   return { data, isLoading, error };
 }
