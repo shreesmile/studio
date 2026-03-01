be6816edaadc92650ce4865df52c0725ebd383b1
RADHA SHREE M <radhashri1999@gmail.com>
2026-03-01 10:56:02 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8879417322-9c80f",
+  "appId": "1:291875744901:web:2825da8b4ad8cf8e478dec",
+  "apiKey": "AIzaSyA0ViL178jpmoVhbBFNm01jA7IpxtKUYfw",
+  "authDomain": "studio-8879417322-9c80f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "291875744901"
+};
